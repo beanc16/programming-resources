947d7387e703ce74740ba7b91d2492ff36bdddf5
beanc16 <bean.christopher@outlook.com>
2021-09-26 15:23:32 -0400

    Update API version to 0.5.0

--- docs/basicInfo.js
+++ docs/basicInfo.js
@@ -3,7 +3,7 @@ module.exports = {
     info: {
       title: "Programming Resources API",
       description: "An API for a living document that will contain various free programming resources to assist with development or help developers learn.",
-      version: "0.4.1",
+      version: "0.5.0",
       contact: {
         name: "Chris Bean", // your name
         email: "bean.christopher@outlook.com", // your email
