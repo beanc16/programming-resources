04f33157eb4ec3733d41a498c13a1db3581b294a
beanc16 <bean.christopher@outlook.com>
2021-09-26 12:06:12 -0400

    Add sort options enum

---
+++ private/js/enums/sortOptionsEnums.js
@@ -0,0 +1,12 @@
+const resourceSortOptions = {
+    "type_name": 1, 
+    "type_order": 1,
+};
+
+Object.freeze(resourceSortOptions);
+
+
+
+module.exports = {
+    "resourceSortOptions": resourceSortOptions,
+};
