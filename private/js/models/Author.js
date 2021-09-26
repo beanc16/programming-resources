f89a9bf00ef565a7208b5cb808e4d747e58fe254
beanc16 <bean.christopher@outlook.com>
2021-09-26 13:54:10 -0400

    Create Author model

---
+++ private/js/models/Author.js
@@ -0,0 +1,18 @@
+class Author
+{
+	constructor(document)
+	{
+		this._id = document._id;
+		this.authorName = document.author_name;
+		this.authorYoutube = document.author_youtube;
+		this.authorWebsite = document.author_website;
+		this.authorTwitter = document.author_twitter;
+		this.authorInstagram = document.author_instagram;
+		this.authorFacebook = document.author_facebook;
+		this.authorLinkedIn = document.author_linkedin;
+		this.authorGithub = document.author_github;
+	}
+}
+
+module.exports = Author;
+
