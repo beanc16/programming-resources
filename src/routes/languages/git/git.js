8e1bf5a5b42f17923a2b4d16b66cd30c9d20f2ee
beanc16 <bean.christopher@outlook.com>
2021-10-02 18:01:42 -0400

    Implement ResourceRoute for Git

--- src/routes/languages/git/git.js
+++ src/routes/languages/git/git.js
@@ -1,18 +1,26 @@
-function Git()
+import ResourceRoute from "../../../components/routes/resourceRoute";
+import VideoLoading from "../../../components/resources/videoLoading";
+
+export default class Git extends ResourceRoute
 {
-    return(
-        <div id="pl-git">
-            <div>
-                <h1>Git</h1>
-                <p>
-                    This will contain quick references and tutorials for 
-                    Git.
-                </p>
+    constructor(props)
+    {
+        super(props, "language", "git");
+    }
+
+    render()
+    {
+        return(
+            <div id="pl-git">
+                <div>
+                    <h1>Git</h1>
+                    <p>
+                        This will contain quick references and tutorials for 
+                        Git.
+                    </p>
+                    {this.state.resources}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-
-
-
-export default Git;
