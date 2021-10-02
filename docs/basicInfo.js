6b1c7fd1ba0109859eef9e0ec4d6f9e5abaa0dc9
beanc16 <bean.christopher@outlook.com>
2021-10-02 16:09:28 -0400

    Update documentation version

--- docs/basicInfo.js
+++ docs/basicInfo.js
@@ -3,7 +3,7 @@ module.exports = {
     info: {
       title: "Programming Resources API",
       description: "An API for a living document that will contain various free programming resources to assist with development or help developers learn.",
-      version: "0.5.2",
+      version: "0.5.3",
       contact: {
         name: "Programming Resources",
         url: "https://beanc16.github.io/programming-resources",
